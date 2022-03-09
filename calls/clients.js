fef9e7024f70418cde30d5ad95475f5aef3507d6
Andreas Georgiou <ageorgiou4@uclan.ac.uk>
2022-03-09 03:41:39 +0200

    Put and Delete Ops

--- calls/clients.js
+++ calls/clients.js
@@ -17,7 +17,7 @@ router.get('/clients', (req, res) => {
     Client.findAll()
         .then(clients => {
             res.status(200)
-            res.setHeader('content-type', 'application/json')
+                .setHeader('content-type', 'application/json')
                 .send(clients); // body is JSON
         })
         .catch(error => {
@@ -33,7 +33,7 @@ router.post('/clients/create', (req, res) => {
     return sequelize.transaction(async (t) => {
         // invalid client posted
         if (!posted_client.name || !posted_client.surname || !posted_client.email || !posted_client.phone || !posted_client.dob) {
-             res.status(422)
+            res.status(422)
                 .setHeader('content-type', 'application/json')
                 .send({error: `Bad request - All fields must be completed`}); // bad request
         }
@@ -52,7 +52,7 @@ router.post('/clients/create', (req, res) => {
             })
             .catch(error => {
                 if (error.name === 'SequelizeUniqueConstraintError') {
-                     res.status(409)
+                    res.status(409)
                         .setHeader('content-type', 'application/json')
                         .send({error: `Client with this email already exists!`}); // resource already exists
                 }
@@ -64,5 +64,90 @@ router.post('/clients/create', (req, res) => {
                 .send({error: `Server error: ${error.name}`});
         });
 });
+
+router.put('/clients/update/:id', (req, res) => {
+    const {id} = req.params; // get id from URI
+    const posted_client = req.body; // submitted client
+
+    return sequelize.transaction(async (t) => {
+        if (isNaN(id)) {
+            return res.status(422)
+                .setHeader('content-type', 'application/json')
+                .send({message: `ID is non-numeric`});
+        }
+
+        const client = await Client.findOne({where: {id: id}})
+
+        if (!client) { // client not found
+            return res.status(404)
+                .setHeader('content-type', 'application/json')
+                .send({error: `Client with id ${id} not found`});
+        }
+
+        // client found
+        if (posted_client)
+            client.name = posted_client.name;
+
+        if (posted_client.surname)
+            client.surname = posted_client.surname;
+
+        if (posted_client)
+            client.email = posted_client.email;
+
+        if (posted_client.phone)
+            client.phone = posted_client.phone;
+
+        if (posted_client.dob)
+            client.dob = posted_client.dob;
+
+        return client.save({transaction: t})
+            .then(client => {
+                res.status(200)
+                    .setHeader('content-type', 'application/json')
+                    .send({message: `Client updated`, client: client}); // body is JSON
+            })
+            .catch(error => {
+                if (error.name === 'SequelizeUniqueConstraintError') {
+                    res.status(409)
+                        .setHeader('content-type', 'application/json')
+                        .send({error: `Client with this email already exists!`}); // resource already exists
+                }
+            });
+    })
+        .catch(error => {
+            res.status(500)
+                .setHeader('content-type', 'application/json')
+                .send({error: `Server error: ${error.name}`});
+        });
+});
+
+router.delete('/clients/delete/:id', (req, res) => {
+    const {id} = req.params; // get client id from URI
+
+    return sequelize.transaction(async (t) => {
+
+        const client = await Client.findOne({where: {id: id}})
+
+        if (!client) {
+            return res.status(404)
+                .setHeader('content-type', 'application/json')
+                .send({error: `Client not found for id ${id}`});
+        }
+
+        // client found
+        return client.destroy({transaction: t})
+            .then(client => {
+                res.status(200)
+                    .setHeader('content-type', 'application/json')
+                    .send({message: `Client deleted`, client: client});
+            })
+    })
+        .catch(error => {
+            res.status(500)
+                .setHeader('content-type', 'application/json')
+                .send({error: `Server error: ${error.name}`});
+        });
+});
+
 
 module.exports = router;
